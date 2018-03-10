66fdabf509be7092d8caf61001b99233e59ef69e
Senal Kularatne <senalk90@gmail.com>
2018-03-09 22:52:40 -0500

    Make content scrollable and add a loading message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import Searchbox from './Searchbox';
-import { countries } from './countries';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
@@ -9,9 +9,15 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      countries: countries,
+      countries: [],
       searchfield: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(response => response.json())
+    .then(users => this.setState({countries: users}));
   }
 
   onSearchChange = (event) => {
@@ -24,13 +30,20 @@ class App extends Component {
       return country.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
 
-    return (
-      <div className='tc'>
-        <h1 className='f-headline lh-solid'>Country Report</h1>
-        <Searchbox searchChange={this.onSearchChange}/>
-        <CardList countries={filteredCountries} />
-      </div>
-    );
+    if (this.state.countries.length === 0) {
+      return <h1>Loading...</h1>
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f-headline lh-solid'>Country Report</h1>
+          <Searchbox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <CardList countries={filteredCountries} />
+          </Scroll>
+        </div>
+      );
+    }
+
   }
 
 }
---
+++ src/Scroll.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Scroll = (props) => {
+  return (
+    <div style={ {margin: '15px', overflowY: 'scroll', border: '.5px solid black', height: '1000px'} }>
+      {props.children}
+    </div>
+  );
+};
+
+export default Scroll;
